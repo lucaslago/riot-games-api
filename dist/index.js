d5d6abd8758c523e7912cb193221ebb0b9ce60c7
Lucas Lago <lucasbenderlago@gmail.com>
2017-04-15 23:48:31 -0300

    Change module entrypoint

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory()
-	: typeof define === 'function' && define.amd ? define(factory)
-	: (global.bundle = factory())
-}(this, function () {
-  'use strict'
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.bundle = factory());
+}(this, (function () { 'use strict';
 
-  const client = {
-    connect: () => {}
-  }
+const client = {
+  connect: () => {}
+};
 
-  var index = {
-    client
-  }
+var index = {
+  client
+};
 
-  return index
-}))
+return index;
+
+})));
